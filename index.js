0e0b7dd0cba0e16a86797aa7a62ff1d18abc36c7
Neematrashid <nymatrasheed05@gmail.com>
2023-09-21 13:28:48 +0300

    arrowfunctions completed

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (no1, no2) => 2000/ 100;
+    //(this the parameter) //this is the function => 
+    //the expression is on the right
+const square = (x) => x *2;
+//multiplies it times 2
+const add = (a,b) => a + b;
